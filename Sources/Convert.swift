d5225ff7623b30021a413f37c6a37120893b3f18
Leon Li <arkadeleon@gmail.com>
2026-09-19 15:16:44 +0800

    Updated bRO client data

--- Sources/Convert.swift
+++ Sources/Convert.swift
@@ -62,7 +62,7 @@ struct Convert: ParsableCommand {
         try converter.convert(from: .txt(input), to: output, for: .it)
         try converter.convert(from: .txt(input), to: output, for: .ja)
         try converter.convert(from: .lua(input, "itemInfo_true.lub"), to: output, for: .ko)
-        try converter.convert(from: .txt(input), to: output, for: .ptBR)
+        try converter.convert(from: .lua(input, "iteminfo_new.lub"), to: output, for: .ptBR, encoding: .utf8)
         try converter.convert(from: .txt(input), to: output, for: .ru)
         try converter.convert(from: .lua(input, "itemInfo_new.lub"), to: output, for: .th, encoding: .utf8)
         try converter.convert(from: .txt(input), to: output, for: .tr)
